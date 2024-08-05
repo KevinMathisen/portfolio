57ed097137d19ed7b8f2a19d158908f43ab8ba22
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-08-05 11:50:17 +0200

    feat: create section component

---
+++ src/lib/section.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Dynamictext from "./dynamictext.svelte";
+	import '../global.css';
+
+	export let title = "";
+</script>
+
+<section>
+	<Dynamictext text={title} />
+
+	<div class="content">
+		<slot />
+	</div>
+</section>
+
+<style>
+	section {
+		margin: 1em;
+		border-bottom: 1px solid var(--PCOLOR);
+	}
+
+	.content {
+		padding: 0 0.2em;
+		padding-bottom: 0.5em;
+	}
+
+</style>
