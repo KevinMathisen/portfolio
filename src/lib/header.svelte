b286ae029625979f6033a92401dc9a85d9ba929c
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-08-05 09:46:47 +0200

    feat: add header

---
+++ src/lib/header.svelte
@@ -0,0 +1,29 @@
+<header>
+	<nav>
+		<a href="/"><img src="/root.svg" alt="Home" width="70" /></a>
+		<a href="/resume.pdf">Resume</a>
+	</nav>
+</header>
+
+<style>
+	header {
+		color: var(--PCOLOR);
+		padding: 2em;
+		text-align: center;
+	}
+
+	nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	nav a {
+		color: var(--PCOLOR);
+		text-decoration: underline;
+		font-weight: bold;
+		font-size: 1.5em;
+		display: flex;
+		align-items: center;
+	}
+</style>
