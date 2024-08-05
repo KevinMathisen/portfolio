2f71237685866ccc21f19f3bff38430b8c1b7e10
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-08-05 11:49:48 +0200

    feat: create component for animating typing text

---
+++ src/lib/dynamictext.svelte
@@ -0,0 +1,69 @@
+<script>
+	import { onMount } from 'svelte';
+	import '../global.css';
+
+	export let text = "";
+
+	let textArray = text.split('');
+	let currentIndex = 0;
+	let currentText = '';
+	let cursorClass = 'blinking-text-cursor';
+
+	onMount(() => {
+		const typeNextCharacter = () => {
+            if (currentIndex < textArray.length) {
+                currentText += textArray[currentIndex];
+                currentIndex++;
+                let interval = Math.random() * 100 + 50;
+                setTimeout(typeNextCharacter, interval);
+            } else {
+                setTimeout(() => {
+                    cursorClass = 'non-blinking-text-cursor';
+                }, 3000);
+            }
+        };
+        typeNextCharacter();
+	});
+</script>
+
+<h1>{currentText}<span class={cursorClass}>|</span></h1>
+
+<style>
+    h1 {
+        font-size: 1.6em;
+        text-align: left;
+        color: var(--PCOLOR);
+		margin: 0.2em 0;
+		letter-spacing: -2%;
+    }
+
+    .blinking-text-cursor {
+        font-weight: 100;
+        font-size: 1.3em;
+        color: var(--PCOLOR);
+        -webkit-animation: blinkingTextCursor 1s steps(1, start) infinite;
+        -moz-animation: blinkingTextCursor 1s steps(1, start) infinite;
+        animation: blinkingTextCursor 1s steps(1, start) infinite;
+    }
+
+	.non-blinking-text-cursor {
+		font-weight: 100;
+        font-size: 1.3em;
+        color: var(--BCOLOR);
+	}
+
+    @keyframes blinkingTextCursor {
+        0%, 49% { opacity: 1; }
+        50%, 100% { opacity: 0; }
+    }
+
+    @-webkit-keyframes blinkingTextCursor {
+        0%, 49% { opacity: 1; }
+        50%, 100% { opacity: 0; }
+    }
+
+    @-moz-keyframes blinkingTextCursor {
+        0%, 49% { opacity: 1; }
+        50%, 100% { opacity: 0; }
+    }
+</style>
