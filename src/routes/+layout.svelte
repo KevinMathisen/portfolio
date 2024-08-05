6ccc8b4cd1fef54ed3e7c858a9d85f7cc6436b71
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-08-05 11:51:26 +0200

    feat: add first section to main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,3 +7,9 @@
 <main>
 	<slot />
 </main>
+
+<style>
+	main {
+		padding: 1em;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Section from "$lib/section.svelte";
+	import General from "$lib/general.svelte";
+	import Boxes from "$lib/boxes.svelte";
+	import '../global.css';
+
+	let languages = ['C', 'C++', 'HTML', 'CSS', 'Javascript', 'Python', 'Golang', 'Kotlin', 'Powershell', 'Bash', 'Assembly', 'SQL', 'YAML'];
+	let svgPaths = ['c.svg', 'cpp.svg', 'html.svg', 'css.svg', 'javascript.svg', 'python.svg', 'golang.svg', 'kotlin.svg', 'powershell.svg', 'bash.svg', 'assembly.svg', 'sql.svg', 'yaml.svg'];
+</script>
+
+<Section title="Kevin Nikolai Mathisen">
+	<General />
+</Section>
+
+
