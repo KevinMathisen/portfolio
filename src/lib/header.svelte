988ebb8d8ea98f5f3ce4201f6d0523c8c39093ff
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-08-06 17:56:13 +0200

    feat: make resume link interactive

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,13 +1,12 @@
 <header>
 	<nav>
-		<a href="/"><img src="/root.svg" alt="Home" width="70" /></a>
-		<a href="/resume.pdf">Resume</a>
+		<a class="homelink" href="/"><img src="/root.svg" alt="Home" width="70" /></a>
+		<a class="resumelink" href="/resume.pdf">Resume</a>
 	</nav>
 </header>
 
 <style>
 	header {
-		color: var(--PCOLOR);
 		padding: 2em;
 		text-align: center;
 	}
@@ -18,12 +17,31 @@
 		align-items: center;
 	}
 
-	nav a {
-		color: var(--PCOLOR);
-		text-decoration: underline;
-		font-weight: bold;
-		font-size: 1.5em;
+	.homelink {
 		display: flex;
 		align-items: center;
+	}
+
+	.resumelink {
+		text-decoration: none;
+		font-weight: bold;
+		font-size: 1.5em;
+		color: var(--PCOLOR);
+
+		background-position-y: -0%;
+		background-image: linear-gradient(180deg, transparent 50%, #EBE8DD 50%) !important;
+		transition: all 0.15s ease;
+		background-size: 2px;
+		background-size: auto 190%;
+	}
+
+	.resumelink:hover {
+		background-position-y: 100%;
+		color: black;
+		transition: all 0.15s ease;
+	}
+
+	.resumelink:active {
+		background-position-y: 90%;
 	}
 </style>
