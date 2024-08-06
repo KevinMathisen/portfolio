5447eaaf9a8fe3554ede2df43bd4888e635dbae2
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2024-08-06 18:27:40 +0200

    feat: add links to projects

--- src/lib/projects.svelte
+++ src/lib/projects.svelte
@@ -4,21 +4,25 @@
 	let projects = [
 		{
 			title: "Webapp for displaying environmental data",
+			link: "https://github.com/KevinMathisen/interactive-database-for-environmental-data",
 			content: "A custom, interactive web application for displaying environmental data for scientists",
 			svgPaths: ["technologies/svelte.svg", "languages/javascript.svg", "technologies/nginx.svg", "technologies/docker.svg", "technologies/postgres.svg"]
 		},
 		{
 			title: "Violetmedia",
+			link: "https://github.com/KevinMathisen/VioletMedia",
 			content: "An android application for streaming and playing videoes from other devices and servers over a local network",
 			svgPaths: ["languages/kotlin.svg"]
 		},
 		{
 			title: "Renewables API ",
+			link: "https://github.com/KevinMathisen/renewable-energy-for-countries-api",
 			content: "An API serving current and historic percentage of renewables in various countries",
 			svgPaths: ["languages/golang.svg", "technologies/docker.svg"]
 		},
 		{
 			title: "Portfolio",
+			link: "",
 			content: "Personal portfolio developed with Svelte and SvelteKit",
 			svgPaths: ["languages/javascript.svg", "technologies/svelte.svg", "technologies/nginx.svg"]
 		}
@@ -27,7 +31,7 @@
 
 <div class="grid">
 	{#each projects as project}
-		<Contentbox title={project.title} content={project.content} svgPaths={project.svgPaths} />
+		<Contentbox title={project.title} content={project.content} svgPaths={project.svgPaths} link={project.link} />
 	{/each}
 </div>
 
